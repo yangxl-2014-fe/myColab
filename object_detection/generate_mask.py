279bb39f1d2b772e4259440c0fcec4113474251d
yangxl (ftxS) <yangxl.2014.fe@gmail.com>
2021-01-27 16:26:54 +0800

    -) 清理未使用的代码.

--- object_detection/generate_mask.py
+++ object_detection/generate_mask.py
@@ -10,9 +10,9 @@ https://colab.research.google.com/drive/16jcaJoc6bCFAQ96jDe2HwtXj7BMD_-m5
 import sys
 import os
 import os.path as osp
-import logging
-import json
-import random
+# import logging
+# import json
+# import random
 import time
 import numpy as np
 import cv2
@@ -30,12 +30,13 @@ from detectron2.utils.logger import setup_logger
 # import some common detectron2 utilities
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
-from detectron2.engine import DefaultTrainer
+# from detectron2.engine import DefaultTrainer
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
+# from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data import MetadataCatalog
 
-from detectron2.structures import BoxMode
+# from detectron2.structures import BoxMode
 
 # User import
 from object_detection.configs import cfg as gcfg
@@ -44,35 +45,11 @@ from object_detection.configs import get_specific_files
 # from object_detection.configs import get_specific_files_with_tag_in_name
 # from object_detection.visualization import viz_image_grid
 
-'''
-def get_image():
-    img_url = 'http://images.cocodataset.org/val2017/000000439715.jpg'
-    img_save = osp.join(gcfg.get_temp_dir, 'input.jpg')
-    if not osp.exists(img_save):
-        subprocess.call(['wget', img_url, '-q', '-O', img_save])
-    im = cv2.imread(img_save)
-    return im
-'''
 
-
+'''
 def get_save_path():
     img_save = osp.join(gcfg.get_temp_dir, 'output.png')
     return img_save
-
-
-'''
-def get_dataset():
-    # download, decompress the data
-    zip_url = ('https://github.com/matterport/Mask_RCNN/releases/download/'
-               'v2.1/balloon_dataset.zip')
-    zip_save = osp.join(gcfg.get_temp_dir, 'balloon_dataset.zip')
-    if not osp.exists(zip_save):
-        subprocess.call(['wget', zip_url, '-O', zip_save])
-
-        cwd_path = os.getcwd()
-        os.chdir(gcfg.get_temp_dir)
-        subprocess.call(['unzip', zip_save])
-        os.chdir(cwd_path)
 '''
 
 
@@ -107,106 +84,6 @@ def predict_image(im):
     return out.get_image()
 
 
-'''
-def get_balloon_dicts(img_dir):
-    """
-    Notes
-    -----
-    if your dataset is in COCO format, this cell can be replaced by the
-        following three lines:
-
-    from detectron2.data.datasets import register_coco_instances
-    register_coco_instances("my_dataset_train", {},
-                            "json_annotation_train.json", "path/to/image/dir")
-    register_coco_instances("my_dataset_val", {},
-                            "json_annotation_val.json", "path/to/image/dir")
-    """
-    json_file = os.path.join(img_dir, "via_region_data.json")
-    with open(json_file) as f:
-        imgs_anns = json.load(f)
-
-    dataset_dicts = []
-    for idx, v in enumerate(imgs_anns.values()):
-        record = {}
-
-        filename = os.path.join(img_dir, v["filename"])
-        height, width = cv2.imread(filename).shape[:2]
-
-        record["file_name"] = filename
-        record["image_id"] = idx
-        record["height"] = height
-        record["width"] = width
-
-        annos = v["regions"]
-        objs = []
-        for _, anno in annos.items():
-            assert not anno["region_attributes"]
-            anno = anno["shape_attributes"]
-            px = anno["all_points_x"]
-            py = anno["all_points_y"]
-            poly = [(x + 0.5, y + 0.5) for x, y in zip(px, py)]
-            poly = [p for x in poly for p in x]
-
-            obj = {
-                "bbox": [np.min(px), np.min(py), np.max(px), np.max(py)],
-                "bbox_mode": BoxMode.XYXY_ABS,
-                "segmentation": [poly],
-                "category_id": 0,
-            }
-            objs.append(obj)
-        record["annotations"] = objs
-        dataset_dicts.append(record)
-    return dataset_dicts
-
-
-def detect_balloon():
-    for d in ["train", "val"]:
-        DatasetCatalog.register(
-            "balloon_" + d,
-            lambda d=d: get_balloon_dicts(
-                gcfg.get_temp_dir + '/' + "balloon/" + d))
-        MetadataCatalog.get("balloon_" + d).set(thing_classes=["balloon"])
-    balloon_metadata = MetadataCatalog.get("balloon_train")
-
-    dataset_dicts = get_balloon_dicts(gcfg.get_temp_dir + '/' + "balloon/train")
-    for d in random.sample(dataset_dicts, 3):
-        img = cv2.imread(d["file_name"])
-        visualizer = Visualizer(img[:, :, ::-1],
-                                metadata=balloon_metadata, scale=0.5)
-        out = visualizer.draw_dataset_dict(d)
-        save_path = osp.join(gcfg.get_temp_dir, osp.basename(d["file_name"]))
-        cv2.imwrite(save_path, out.get_image())
-
-
-def train_model():
-    cfg = get_cfg()
-    cfg.merge_from_file(model_zoo.get_config_file(
-        "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-    cfg.DATASETS.TRAIN = ("balloon_train",)
-    cfg.DATASETS.TEST = ()
-    cfg.DATALOADER.NUM_WORKERS = 2
-
-    # Let training initialize from model zoo
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
-        "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
-    cfg.SOLVER.IMS_PER_BATCH = 2
-    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-
-    # 300 iterations seems good enough for this toy dataset;
-    #   you may need to train longer for a practical dataset
-    cfg.SOLVER.MAX_ITER = 300
-
-    # faster, and good enough for this toy dataset (default: 512)
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon)
-
-    cfg.OUTPUT_DIR = gcfg.get_ou_dir
-    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-    trainer = DefaultTrainer(cfg)
-    trainer.resume_or_load(resume=False)
-    trainer.train()
-'''
-
 ################################################################################
 # main
 ################################################################################
@@ -221,32 +98,6 @@ if __name__ == '__main__':
     subprocess.call(['gcc', '--version'])
 
     time_beg = time.time()
-
-    # Run a pre-trained detectron2 model
-    '''
-    enable_use_pre_trained = True
-    if enable_use_pre_trained:
-        img_in = get_image()
-        img_ou = predict_image(img_in)
-        cv2.imwrite(get_save_path(), img_ou)
-    '''
-
-    # Train on a custom dataset
-    '''
-    enable_detect_balloon = False
-    if enable_detect_balloon:
-        get_dataset()
-        detect_balloon()
-    '''
-
-    # fine-tune a COCO-pretrained R50-FPN Mask R-CNN model
-    #   on the balloon dataset
-    '''
-    enable_fine_tune = False
-    if enable_fine_tune:
-        # nvidia-smi --loop=2
-        train_model()
-    '''
 
     enable_process_dataset = True
     if enable_process_dataset:
